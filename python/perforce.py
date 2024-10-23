98ad7e8f9033f386a30952415486245c3406e955
Stefan Vidakovic <stefan.vidakovic@inflexion.io>
2024-10-23 10:29:37 -0600

    Temp commit: Set partitioned to default.

--- python/perforce.py
+++ python/perforce.py
@@ -33,7 +33,7 @@ class P4Repo:
         self.sync_paths = sync or ['//...']
         assert isinstance(self.sync_paths, list)
         self.client_options = client_options or ''
-        self.client_type = client_type or 'writeable'
+        self.client_type = client_type or 'partitioned'
         self.parallel = parallel
         self.fingerprint = fingerprint or ''
 
